a24861a851fecc4795225df93cf4c57b29abc0e2
Stephen Green <SGreen@qicglobal.com>
2019-12-05 17:03:48 +0000

    upload

---
+++ Upload/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Upload.Data;
+
+[assembly: HostingStartup(typeof(Upload.Areas.Identity.IdentityHostingStartup))]
+namespace Upload.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+            });
+        }
+    }
+}
